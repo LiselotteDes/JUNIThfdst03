97a00be2b2cb5a9b58541ef322707bdfdc955220
Liselotte <liselotte.des@gmail.com>
2018-02-14 08:21:50 +0100

    test fixture en @before toegevoegd

--- src/test/java/be/vdab/entities/RekeningTest.java
+++ src/test/java/be/vdab/entities/RekeningTest.java
@@ -1,23 +1,33 @@
 package be.vdab.entities;
 import static org.junit.Assert.*;
 import java.math.BigDecimal;
+import org.junit.Before;
 import org.junit.Test;
 public class RekeningTest {
+	// Declareert een private variabele per test fixture
+	private Rekening rekening;
+	/*
+	 * Initialiseert de variabele in een @Before method.
+	 * JUnit voert voor elke @Test method de @Before method uit.
+	 * Je kan nu in @Test methods deze private variabele rekening gebruiken. 
+	 * JUnit initialiseert deze variabele bij elke oproep vd @Before method met een Rekening object.
+	 */
+	@Before
+	public void before() {
+		rekening = new Rekening();
+	}
 	@Test
 	public void hetSaldoVanEenNieuweRekeningIsNul() {
-		Rekening rekening = new Rekening();
 		assertEquals(0, BigDecimal.ZERO.compareTo(rekening.getSaldo()));
 	}
 	@Test
 	public void hetSaldoNaEenSortingIsHetBedragVanDieStorting() {
-		Rekening rekening = new Rekening();
 		BigDecimal bedrag = BigDecimal.valueOf(2.5);
 		rekening.storten(bedrag);
 		assertEquals(0, bedrag.compareTo(rekening.getSaldo()));
 	}
 	@Test
 	public void hetSaldoNaTweeStortingenIsDeSomVanDeBedragenVanDieStortingen() {
-		Rekening rekening = new Rekening();
 		rekening.storten(BigDecimal.valueOf(2.5));
 		rekening.storten(BigDecimal.valueOf(1.2));
 		assertEquals(0,BigDecimal.valueOf(3.7).compareTo(rekening.getSaldo()));
