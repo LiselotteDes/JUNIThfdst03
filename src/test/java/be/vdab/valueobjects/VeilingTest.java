939f351f2a82a1858151b70bcbf61fd79c0af297
Liselotte <liselotte.des@gmail.com>
2018-02-14 08:28:24 +0100

    test fixture en @before toegevoegd aan taak Veiling

--- src/test/java/be/vdab/valueobjects/VeilingTest.java
+++ src/test/java/be/vdab/valueobjects/VeilingTest.java
@@ -1,25 +1,27 @@
 package be.vdab.valueobjects;
 
 import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class VeilingTest {
-
+	private Veiling veiling;
+	@Before
+	public void before() {
+		veiling = new Veiling();
+	}
+	
 	@Test
 	public void hetHoogsteBodVanEenNieuweVeilingIsNul() {
-		Veiling veiling = new Veiling();
 		assertEquals(0, veiling.getHoogsteBod());
 	}
 	@Test
 	public void naEersteBodIsHetHoogsteBodGelijkAanHetBedragVanDitBod() {
-		Veiling veiling = new Veiling();
 		veiling.doeBod(100);
 		assertEquals(100, veiling.getHoogsteBod());
 	}
 	@Test
 	public void hetHoogsteBodNaEerst100Dan200Dan150Is200() {
-		Veiling veiling = new Veiling();
 		veiling.doeBod(100);
 		veiling.doeBod(200);
 		veiling.doeBod(150);
